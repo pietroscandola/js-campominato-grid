ec95cc121a9f86ee9e9af0ed46c2bb61cfcd43c3
pietroscandola <pietro.scandolavr@gmail.com>
2022-01-17 15:27:11 +0100

    add functions

--- js/script.js
+++ js/script.js
@@ -5,3 +5,33 @@ con difficoltà 2 => tra 1 e 81
 con difficoltà 3 => tra 1 e 49
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 */
+
+//* FUNZIONI DA UTILIZZARE
+
+function getUniqueRndNumber(min, max, list) {
+    let rndNumber;
+    do {
+        console.log(rndNumber);
+        rndNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    } while (list.includes(rndNumber));
+    return rndNumber;
+}
+
+function createCell(cellNumber) {
+    const cell = document.createElement('div');
+    cell.className = 'cell';
+    cell.id = cellNumber;
+    cell.innerText = cellNumber;
+    return cell;
+}
+
+
+
+
+// SE SI SCEGLIE DIFFICOLTA 1
+
+const cell = 10;
+const column = 10;
+
+const total = cell * column;
+console.log(total);
